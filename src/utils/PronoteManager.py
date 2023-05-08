98904c4df5f537f0cb1ca9d4f92ec9717fd3977b
-Lucas <lucas.llasbennes@gmail.com>
2023-05-08 23:24:18 +0200

    csv importation base

---
+++ src/utils/PronoteManager.py
@@ -0,0 +1,17 @@
+import csv
+import os
+
+
+def importCsv():
+    table = []
+    #csv.reader(open('csv_files/sample.csv', 'rU'), delimiter=",", quotechar='|')
+    with open(os.path.join("resources", "resultatVF.csv"), 'rU') as csvfile:  # ouverture du fichier csv
+        lecture_fichier_csv = csv.reader(csvfile,
+                                         delimiter=',')  # stockage dans la variable lecture_fichier_csv avec délimiteur "virgule"
+
+        for ligne in lecture_fichier_csv:
+            table.append(ligne)
+    print(table)
+
+
+importCsv()
