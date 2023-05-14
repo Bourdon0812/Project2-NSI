a4b65a205f1f00d0eb7155acd603efe3f90db0cb
-Lucas <lucas.llasbennes@gmail.com>
2023-05-14 23:58:45 +0200

    CSV Open & Files modifications

--- src/utils/PronoteManager.py
+++ src/utils/PronoteManager.py
@@ -1,14 +1,10 @@
 import csv
-import os
-
 
 def importCsv():
     table = []
     #csv.reader(open('csv_files/sample.csv', 'rU'), delimiter=",", quotechar='|')
-    with open(os.path.join("resources", "resultatVF.csv"), 'rU') as csvfile:  # ouverture du fichier csv
-        lecture_fichier_csv = csv.reader(csvfile,
-                                         delimiter=',')  # stockage dans la variable lecture_fichier_csv avec délimiteur "virgule"
-
+    with open('resultatVF.csv') as csvfile:  # ouverture du fichier csv
+        lecture_fichier_csv = csv.reader(csvfile,delimiter=',')  # stockage dans la variable lecture_fichier_csv avec délimiteur "virgule"
         for ligne in lecture_fichier_csv:
             table.append(ligne)
     print(table)
