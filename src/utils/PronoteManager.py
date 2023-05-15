b83b67c55987ffc14657412d1cc62dedee97f483
Bourdon <ilan.bonneville@gmail.com>
2023-05-15 11:13:44 +0200

    csv in resources

--- src/utils/PronoteManager.py
+++ src/utils/PronoteManager.py
@@ -3,7 +3,7 @@ import csv
 def importCsv():
     table = []
     #csv.reader(open('csv_files/sample.csv', 'rU'), delimiter=",", quotechar='|')
-    with open('../resources/resultatVF.csv') as csvfile:  # ouverture du fichier csv
+    with open('../../resources/resultatVF.csv') as csvfile:  # ouverture du fichier csv
         lecture_fichier_csv = csv.reader(csvfile,delimiter=',')  # stockage dans la variable lecture_fichier_csv avec délimiteur "virgule"
         for ligne in lecture_fichier_csv:
             table.append(ligne)
